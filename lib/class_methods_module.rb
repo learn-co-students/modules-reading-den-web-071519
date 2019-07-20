f39d54d89e66d6c25c341cd13aadae19afe51b89
jollyjerr <jollyjerr@gmail.com>
2019-07-20 12:28:06 -0600

    Done.

---
+++ lib/class_methods_module.rb
@@ -0,0 +1,5 @@
+module Metadancing
+    def metadata
+        "This class produces objects that love to dance"
+    end
+end
--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,10 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+class Dancer
+    include FancyDance::InstanceMethods #"include" references instance methods
+    extend FancyDance::ClassMethods #while "extend" references class methods
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+    end
+end
